7ab688d0333b74cdf0c5a46b20b19759ca818e16
t0m2031 <40539711+t0m2031@users.noreply.github.com>
2020-04-21 12:03:05 +0530

    Create EM.py

---
+++ EM.py
@@ -0,0 +1,259 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+# For plotting
+import matplotlib.pyplot as plt
+import seaborn as sns
+sns.set_style("white")
+get_ipython().run_line_magic('matplotlib', 'inline')
+#for matrix math
+import numpy as np
+#for normalization + probability density function computation
+from scipy import stats
+#for data preprocessing
+import pandas as pd
+from math import sqrt, log, exp, pi
+from random import uniform
+print("import done")
+
+
+
+# In[93]:
+
+
+random_seed=36788765
+np.random.seed(random_seed)
+
+Mean1 = 2.0  # Input parameter, mean of first normal probability distribution
+Standard_dev1 = 5.0 #@param {type:"number"}
+Mean2 = 10.0 # Input parameter, mean of second normal  probability distribution
+Standard_dev2 = 8.0 #@param {type:"number"}
+Mean3 = 6.0 # Input parameter, mean of second normal  probability distribution
+Standard_dev3 = 8.0 #@param {type:"number"}
+
+# generate data
+y1 = np.random.normal(Mean1, Standard_dev1, 1500)
+y2 = np.random.normal(Mean2, Standard_dev2, 1400)
+y3 = np.random.normal(Mean3, Standard_dev3, 1300)
+
+a=np.append(y1,y2)
+data=np.append(a,y3)
+
+# For data visiualisation calculate left and right of the graph
+Min_graph = min(data)
+Max_graph = max(data)
+x = np.linspace(Min_graph, Max_graph, 2000) # to plot the data
+
+print('Input Gaussian {:}: μ = {:.2}, σ = {:.2}'.format("1", Mean1, Standard_dev1))
+print('Input Gaussian {:}: μ = {:.2}, σ = {:.2}'.format("2", Mean2, Standard_dev2))
+print('Input Gaussian {:}: μ = {:.2}, σ = {:.2}'.format("3", Mean3, Standard_dev3))
+
+sns.distplot(data, bins=20, kde=False);
+
+
+# In[94]:
+
+
+class Gaussian:
+    "Model univariate Gaussian"
+    def __init__(self, mu, sigma):
+        #mean and standard deviation
+        self.mu = mu
+        self.sigma = sigma
+
+    #probability density function
+    def pdf(self, datum):
+        "Probability of a data point given the current parameters"
+        u = (datum - self.mu) / abs(self.sigma)
+        y = (1 / (sqrt(2 * pi) * abs(self.sigma))) * exp(-u * u / 2)
+        return y
+    
+    def __repr__(self):
+        return 'Gaussian({0:4.6}, {1:4.6})'.format(self.mu, self.sigma)
+print("done")
+
+
+# In[95]:
+
+
+#gaussian of best fit
+best_single = Gaussian(np.mean(data), np.std(data))
+print('Best single Gaussian: μ = {:.2}, σ = {:.2}'.format(best_single.mu, best_single.sigma))
+#fit a single gaussian curve to the data
+g_single = stats.norm(best_single.mu, best_single.sigma).pdf(x)
+sns.distplot(data, bins=20, kde=False, norm_hist=True);
+plt.plot(x, g_single, label='single gaussian');
+plt.legend();
+
+
+# In[96]:
+
+
+class GaussianMixture_self:
+    "Model mixture of two univariate Gaussians and their EM estimation"
+
+    def __init__(self, data, mu_min=min(data), mu_max=max(data), sigma_min=1, sigma_max=1, mix=.5):
+        self.data = data
+        #todo the Algorithm would be numerical enhanced by normalizing the data first, next do all the EM steps and do the de-normalising at the end
+        
+        #init with multiple gaussians
+        self.one = Gaussian(uniform(mu_min, mu_max), 
+                            uniform(sigma_min, sigma_max))
+        self.two = Gaussian(uniform(mu_min, mu_max), 
+                            uniform(sigma_min, sigma_max))
+        self.three = Gaussian(uniform(mu_min, mu_max), 
+                            uniform(sigma_min, sigma_max))
+        
+        #as well as how much to mix them
+        self.mix = mix
+
+    def Estep(self):
+        "Perform an E(stimation)-step, assign each point to gaussian 1 or 2 with a percentage"
+        # compute weights
+        self.loglike = 0. # = log(p = 1)
+        for datum in self.data:  
+            # unnormalized weights
+            wp1 = self.one.pdf(datum) * self.mix
+            wp2 = self.two.pdf(datum) * self.mix
+            wp3 = self.three.pdf(datum) * (1. - self.mix)
+            # compute denominator
+            den = wp1 + wp2 + wp3
+            # normalize
+            wp1 /= den   
+            wp2 /= den
+            wp3 /= den # wp1+wp2= 1, it either belongs to gaussian 1 or gaussion 2
+            # add into loglike
+            self.loglike += log(den) #freshening up self.loglike in the process
+            # yield weight tuple
+            yield (wp1, wp2, wp3)
+
+    def Mstep(self, weights):
+        "Perform an M(aximization)-step"
+        # compute denominators
+        (left, rigt, centre) = zip(*weights) 
+        one_den = sum(left)
+        two_den = sum(rigt)
+        three_den = sum(centre)
+
+        # compute new means
+        self.one.mu = sum(w * d  for (w, d) in zip(left, data)) / one_den
+        self.two.mu = sum(w * d  for (w, d) in zip(rigt, data)) / two_den
+        self.three.mu = sum(w * d  for (w, d) in zip(centre, data)) / three_den
+        
+        # compute new sigmas
+        self.one.sigma = sqrt(sum(w * ((d - self.one.mu) ** 2)
+                                  for (w, d) in zip(left, data)) / one_den)
+        self.two.sigma = sqrt(sum(w * ((d - self.two.mu) ** 2)
+                                  for (w, d) in zip(rigt, data)) / two_den)
+        self.three.sigma = sqrt(sum(w * ((d - self.three.mu) ** 2)
+                                  for (w, d) in zip(centre, data)) / three_den)
+        # compute new mix
+        self.mix = one_den / len(data)
+
+        
+    def iterate(self, N=1, verbose=False):
+        "Perform N iterations, then compute log-likelihood"
+        for i in range(1, N+1):
+            self.Mstep(self.Estep()) #The heart of the algorith, perform E-stepand next M-step
+            if verbose:
+                print('{0:2} {1}'.format(i, self))
+        self.Estep() # to freshen up self.loglike
+
+    def pdf(self, x):
+        return (self.mix)*self.one.pdf(x) + (self.mix)*self.two.pdf(x) + (1-self.mix)*self.three.pdf(x)
+        
+    def __repr__(self):
+        return 'GaussianMixture({0}, {1}, mix={2.03})'.format(self.one, 
+                                                              self.two,
+                                                              self.mix)
+
+    def __str__(self):
+        return 'Mixture: {0}, {1}, mix={2:.03})'.format(self.one,
+                                                        self.two,
+                                                        self.mix)
+print("done")
+
+
+# In[97]:
+
+
+# See the algorithem in action
+n_iterations = 30
+best_mix = None
+best_loglike = float('-inf')
+mix = GaussianMixture_self(data)
+for _ in range(n_iterations):
+    try:
+        #train!
+        mix.iterate(verbose=True)
+        if mix.loglike > best_loglike:
+            best_loglike = mix.loglike
+            best_mix = mix
+        
+    except (ZeroDivisionError, ValueError, RuntimeWarning): # Catch division errors from bad starts, and just throw them out...
+        print("one less")
+        pass
+
+
+# In[98]:
+
+
+# Find best Mixture Gaussian model
+n_iterations = 300
+n_random_restarts = 4
+best_mix = None
+best_loglike = float('-inf')
+print('Computing best model with random restarts...\n')
+for _ in range(n_random_restarts):
+    mix = GaussianMixture_self(data)
+    for _ in range(n_iterations):
+        try:
+            mix.iterate()
+            if mix.loglike > best_loglike:
+                best_loglike = mix.loglike
+                best_mix = mix
+        except (ZeroDivisionError, ValueError, RuntimeWarning): # Catch division errors from bad starts, and just throw them out...
+            pass
+#print('Best Gaussian Mixture : μ = {:.2}, σ = {:.2} with μ = {:.2}, σ = {:.2}'.format(best_mix.one.mu, best_mix.one.sigma, best_mix.two.mu, best_mix.two.sigma))
+print('Input Gaussian {:}: μ = {:.2}, σ = {:.2}'.format("1", Mean1, Standard_dev1))
+print('Input Gaussian {:}: μ = {:.2}, σ = {:.2}'.format("2", Mean2, Standard_dev2))
+print('Input Gaussian {:}: μ = {:.2}, σ = {:.2}'.format("3", Mean3, Standard_dev3))
+print('Gaussian {:}: μ = {:.2}, σ = {:.2}, weight = {:.2}'.format("1", best_mix.one.mu, best_mix.one.sigma, best_mix.mix))
+print('Gaussian {:}: μ = {:.2}, σ = {:.2}, weight = {:.2}'.format("2", best_mix.two.mu, best_mix.two.sigma, best_mix.mix))
+print('Gaussian {:}: μ = {:.2}, σ = {:.2}, weight = {:.2}'.format("3", best_mix.three.mu, best_mix.three.sigma, (1-best_mix.mix)))
+#Show mixture
+sns.distplot(data, bins=20, kde=False, norm_hist=True);
+g_both = [best_mix.pdf(e) for e in x]
+plt.plot(x, g_both, label='gaussian mixture');
+g_left = [best_mix.one.pdf(e) * best_mix.mix for e in x]
+plt.plot(x, g_left, label='gaussian one');
+g_right = [best_mix.two.pdf(e) * best_mix.mix for e in x]
+plt.plot(x, g_right, label='gaussian two');
+g_centre = [best_mix.three.pdf(e) * (1-best_mix.mix) for e in x]
+plt.plot(x, g_centre, label='gaussian three');
+plt.legend();
+
+
+# In[99]:
+
+
+from sklearn.mixture import GaussianMixture
+gmm = GaussianMixture(n_components = 2, tol=0.000001)
+gmm.fit(np.expand_dims(data, 1)) # Parameters: array-like, shape (n_samples, n_features), 1 dimension dataset so 1 feature
+Gaussian_nr = 1
+print('Input Gaussian {:}: μ = {:.2}, σ = {:.2}'.format("1", Mean1, Standard_dev1))
+print('Input Gaussian {:}: μ = {:.2}, σ = {:.2}'.format("2", Mean2, Standard_dev2))
+print('Input Gaussian {:}: μ = {:.2}, σ = {:.2}'.format("3", Mean3, Standard_dev3))
+for mu, sd, p in zip(gmm.means_.flatten(), np.sqrt(gmm.covariances_.flatten()), gmm.weights_):
+    print('Gaussian {:}: μ = {:.2}, σ = {:.2}, weight = {:.2}'.format(Gaussian_nr, mu, sd, p))
+    g_s = stats.norm(mu, sd).pdf(x) * p
+    plt.plot(x, g_s, label='gaussian sklearn');
+    Gaussian_nr += 1
+sns.distplot(data, bins=20, kde=False, norm_hist=True)
+gmm_sum = np.exp([gmm.score_samples(e.reshape(-1, 1)) for e in x]) #gmm gives log probability, hence the exp() function
+plt.plot(x, gmm_sum, label='gaussian mixture');
+plt.legend();
+
